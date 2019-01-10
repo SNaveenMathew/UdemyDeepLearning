f87edb6cbe16475b5d5b58bc91cf52a13052a0f2
SNaveenMathew <pg13s_nathan@mandevian.com>
2019-01-10 16:33:03 -0600

    Adding all files related to TSNE, adding required functions for simulating donut, xor, gauss cloud, etc

--- Unsupervised Deep Learning in Python/util.py
+++ Unsupervised Deep Learning in Python/util.py
@@ -14,3 +14,45 @@ def get_MNIST():
 	
 	return X_train, Y_train, X_test, Y_test
 
+def get_donut_data():
+	N = 500
+	rad_inner = 10
+	rad_outer = 20
+	r1 = rad_inner + np.random.randn(int(N/2))
+	theta = 2 * np.pi * np.random.random(int(N/2))
+	x_inner = np.concatenate([[r1 * np.cos(theta)], [r1 * np.sin(theta)]]).T
+	r2 = rad_outer + np.random.randn(int(N/2))
+	theta = 2 * np.pi * np.random.random(int(N/2))
+	x_outer = np.concatenate([[r2 * np.cos(theta)], [r2 * np.sin(theta)]]).T
+	
+	X = np.concatenate([x_inner, x_outer])
+	Y = np.array([0] * int(N/2) + [1] * int(N/2))
+	return X, Y
+
+def get_xor_data():
+	x1 = np.random.random((100, 2))
+	x2 = np.random.random((100, 2)) - np.array([1, 1])
+	x3 = np.random.random((100, 2)) - np.array([1, 0])
+	x4 = np.random.random((100, 2)) - np.array([0, 1])
+	x = np.vstack([x1, x2, x3, x4])
+	y = np.array([0] * 200 + [1] * 200)
+	return x, y
+
+def get_gauss_cloud(pts_per_cloud = 50):
+	centers = []
+	x = [0, 2]
+	for i in x:
+		for j in x:
+			for k in x:
+				centers.append([i, j, k])
+
+	centers = np.array(centers)
+	dat = []
+
+	for ctr in centers:
+		cloud = np.random.randn(pts_per_cloud, 3) * 0.5 + ctr
+		dat.append(cloud)
+	
+	dat = np.concatenate(dat)
+	return centers, dat
+
